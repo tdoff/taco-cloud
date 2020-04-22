368ff402402b3fd5800da68a926811fce3a6abd3
tdoff <Nalr1meT190>
2020-04-22 08:10:42 +0600

    ch04 beginning

---
+++ ch04/tacos/src/main/java/tacos/data/OrderRepository.java
@@ -0,0 +1,15 @@
+package tacos.data;
+
+import org.springframework.data.repository.CrudRepository;
+import tacos.Order;
+
+import java.util.Date;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+
+    List<Order> findByDeliveryZip(String deliveryZip);
+
+    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+}
---
+++ ch04/tacos/src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,86 @@
+package tacos.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import tacos.Ingredient;
+import tacos.Order;
+import tacos.Taco;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static tacos.Ingredient.Type;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("order")
+public class DesignTacoController {
+
+    private final IngredientRepository ingredientRepo;
+
+    private final TacoRepository designRepo;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository designRepo) {
+        this.ingredientRepo = ingredientRepo;
+        this.designRepo = designRepo;
+    }
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepo.findAll().forEach(ingredients::add);
+
+        Type[] types = Ingredient.Type.values();
+
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+        return "design";
+    }
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors,
+                                @ModelAttribute Order order) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        Taco saved = designRepo.save(design);
+        order.addDesign(saved);
+
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(ing -> ing.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+
+}
